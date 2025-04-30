ad8b062ea2b10942010aacc2db12e51a7da796b5
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2025-04-30 13:53:36 +0200

    Update _document.tsx
    favicon url fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="https://spi-u.intranet.bs.ch/JSD/QMServices/Roadmap/roadmapapp/favicon.ico" sizes="any" />
         </Head>
         <body>
           <Main />
